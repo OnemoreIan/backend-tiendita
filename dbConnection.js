7e483e9a81e63d9b7ec1afe0de32d51586b24bf3
Ian <iandelrazo@gmail.com>
2023-04-24 14:30:30 -0600

    pre produccion

--- dbConnection.js
+++ dbConnection.js
@@ -1,12 +1,11 @@
 //Conexion con base de datos
 const mongoose = require('mongoose');
 require('dotenv').config();
-const permiso = process.env.PERMISO;
-const urlProduccion = `mongodb+srv://mondongo:${permiso}@cluster0.bjafrn2.mongodb.net/?retryWrites=true&w=majority`
-const urlDesarrollo = 'mongodb://127.0.0.1:27017/shop';
+const urlProduccion = process.env.CLOUD;
+const urlDesarrollo = process.env.LOCAL;
 const db = mongoose.connection;
 
-mongoose.connect(urlDesarrollo, {useNewUrlParser: true,useUnifiedTopology: true,})
+mongoose.connect(urlProduccion, {useNewUrlParser: true,useUnifiedTopology: true,})
     .catch(e => console.log(e));
 
 db.once('open', () => {
